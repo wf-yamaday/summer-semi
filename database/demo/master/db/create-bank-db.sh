a0d57c1499777ed7e3c86929dbd368b0485d51d3
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-07-22 14:34:57 +0900

    feat: トランザクションの演習用のデータを用意

---
+++ database/demo/master/db/create-bank-db.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/sh
+
+echo "CREATE DATABASE IF NOT EXISTS \`bank_db\`;" | mysql -u root -p"$MYSQL_ROOT_PASSWORD"
+echo "GRANT ALL ON \`bank_db\`.* TO 'root'@'%' ;" | mysql -u root -p"$MYSQL_ROOT_PASSWORD"
+
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" bank_db < /db/bank.sql
