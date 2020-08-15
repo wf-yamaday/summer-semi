3fdbcdd8e706b8afe2f5be22fd3573f604d5de75
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-08-15 21:19:47 +0900

    feat: 全文検索用のデータのsqlと投入スクリプトの用意

---
+++ database/demo/master/db/create-fulltext-db.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/sh
+
+echo "CREATE DATABASE IF NOT EXISTS \`fulltext_db\`;" | mysql -u root -p"$MYSQL_ROOT_PASSWORD"
+echo "GRANT ALL ON \`fulltext_db\`.* TO 'root'@'%' ;" | mysql -u root -p"$MYSQL_ROOT_PASSWORD"
+
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" fulltext_db < /db/fulltext.sql
---
+++ database/demo/master/db/fulltext.sql
@@ -0,0 +1,15 @@
+﻿DROP TABLE IF EXISTS `documents`;
+
+create table `documents` (
+  `id` INT unsigned NOT NULL AUTO_INCREMENT,
+  `content` text,
+  PRIMARY KEY (`id`),
+  FULLTEXT(`content`) WITH PARSER ngram
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `documents` (`content`)
+VALUES
+  ('春はあけぼの。やうやう白くなりゆく山際、少し明かりて、紫だちたる雲の細くたなびきたる。'),
+  ('夏は夜。月のころはさらなり、闇もなほ、蛍の多く飛びちがひたる。また、ただ一つ二つなど、ほのかにうち光て行くもをかし。雨など降るもをかし。'),
+  ('秋は夕暮れ。夕日の差して山の端いと近うなりたるに、烏の寝所へ行くとて、三つ四つ、二つ三つなど飛び急ぐさへあはれなり。まいて雁などの連ねたるが、いと小さく見ゆるは、いとをかし。日入り果てて、風の音、虫の音など、はた言ふべきにあらず。'),
+  ('冬はつとめて。雪の降りたるは言ふべきにもあらず、霜のいと白きも、またさらでもいと寒きに、火など急ぎおこして、炭持て渡るも、いとつきづきし。昼になりて、ぬるくゆるびもていけば、火桶の火も、白き灰がちになりてわろし。');
